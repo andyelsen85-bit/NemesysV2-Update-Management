5d9c7e2eabe1e15c2e3e1bde70bbab538e4ca871
andyelsen85-bit <andyelsen85@gmail.com>
2026-09-02 11:28:57 +0000

    Fix WiX MSI version preprocessor constant

--- installer/windows/build-msi.ps1
+++ installer/windows/build-msi.ps1
@@ -9,6 +9,6 @@ $client = Join-Path $root "clients\windows-service"
 $project = Join-Path $PSScriptRoot "NemesysV2.Client.wixproj"
 
 dotnet publish $client -c $Configuration -r win-x64 --self-contained true -p:PublishSingleFile=true
-dotnet build $project -c $Configuration -p:MsiVersion=$Version -p:DefineConstants="ClientPublishDir=$client\bin\$Configuration\net8.0-windows\win-x64\publish"
+dotnet build $project -c $Configuration -p:MsiVersion=$Version -p:DefineConstants="MsiVersion=$Version;ClientPublishDir=$client\bin\$Configuration\net8.0-windows\win-x64\publish"
 
 Write-Host "MSI created under installer\windows\bin\$Configuration"
